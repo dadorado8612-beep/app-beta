726dab4244b18bdfb4175e1017af84cbf4912523
Dorado <dadorado.8612@unicesmag.edu.co>
2026-03-25 08:38:49 -0500

    seuimos avanzando

--- config/database.sql
+++ config/database.sql
@@ -1,10 +1,2 @@
-postgres=# create database app_beta;
-CREATE DATABASE
-postgres=# \c app_beta
-Ahora está conectado a la base de datos «app_beta» con el usuario «postgres».
-app_beta=# create table users (nombre varchar(100),apellido varchar(100),telefono varchar(20), email varchar(100));
-CREATE TABLE
-app_beta=# alter table users add column contraseña varchar(100);
-ALTER TABLE
-app_beta=# alter table users add column id char(100);
-ALTER TABLE
+create table users
+(nombre varchar(100),apellido(100),telefono varchar(20), email varchar(100),contraseña varchar(100),id char (100));
--- src/signup.php
+++ src/signup.php
@@ -6,6 +6,7 @@ $l_name = $_POST ['lname'];
 $m_phone = $_POST ['mphone'];
 $e_mail= $_POST ['email'];
 $p_sswd= $_POST ['passwd'];
-$sql ="INSERT INTO users (nombre,apellido,telefono,email,contraseña) VALUES ('$f_name','$l_name','$m_phone','$e_mail','$p_sswd')";
+$enc_pass = md5($p_sswd);
+$sql ="INSERT INTO users (nombre,apellido,telefono,email,contrasena) VALUES ('$f_name','$l_name','$m_phone','$e_mail','$enc_pass')";
 pg_query($sql);
 ?>
